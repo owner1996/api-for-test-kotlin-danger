288d8f3bd8438ca0fb2f88ec79b31e850bb19131
Mukhammad Zhalolov <muhammadzalolov@gmail.com>
2023-03-20 18:35:56 +0300

    add bye

--- src/main/java/ru/jalolov/services/apifortestkotlindanger/rest/HelloController.java
+++ src/main/java/ru/jalolov/services/apifortestkotlindanger/rest/HelloController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String all(@RequestParam(defaultValue = "user") String name) {
+    public String hello(@RequestParam(defaultValue = "user") String name) {
         return "Hello, " + name;
+    }
+
+    @GetMapping("/bye")
+    public String bye(@RequestParam(defaultValue = "user") String name) {
+        return "Bye, " + name;
     }
 }
